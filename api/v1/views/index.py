48df18bbc6f782cdad6ca850a4200dd296a6ac97
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-10-25 13:33:55 +0100

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,13 @@
 Script defines url route for the blueprint object
 """
 
-
-from crypt import methods
-from pickle import GET
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -15,7 +19,19 @@ def status_code():
     """
     the url path /status returns a status code 'OK' in json format
     """
-    # status = {
-    #     'status' : 'OK'
-    # }
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', methods=['GET'], strict_slashes=False)
+def retrieve_obj():
+    """
+    retrieves number of object by type
+    """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_obj = {}
+    for i in range(len(classes)):
+        num_obj[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_obj)
