a4e18a566c08f3e7d56f1d402465b457d06b7e71
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-10-25 08:15:59 +0100

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,6 +9,7 @@ from pickle import GET
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status_code():
     """
