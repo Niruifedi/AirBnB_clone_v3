33fb3bb197a3df03adc078ca8716f5e7453b2888
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-10-25 13:42:50 +0100

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Script uses blueprint object for routing the application
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -19,6 +19,14 @@ def teardown_db(error):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(e):
+    """
+    404 error handler
+    """
+    return jsonify({"error": 'Not found'})
+
+
 if __name__ == '__main__':
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
@@ -28,4 +36,4 @@ if __name__ == '__main__':
     if not port:
         port = 5000
 
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True)  # type: ignore
